e8599e8b85ac905137482b3570c6db4d291376f8
haitan2408 <haitan28102408@gmail.com>
2023-03-07 10:14:16 +0700

    up demo bài tổng quan JS

---
+++ ss6/demo_js.js
@@ -0,0 +1,7 @@
+//changeText giúp thay đổi nội dung và màu chữ
+function changeText() {
+    let toan = 5;
+    document.getElementById("c02").style.color = "red";
+    document.getElementById("c02").innerText = "Bye C02";
+    let van = 7;
+}
